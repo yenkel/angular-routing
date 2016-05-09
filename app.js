dcf51c5ad1a4838cb00218b89d2113aea84c957d
amhayslip <amhayslip@gmail.com>
2016-05-09 20:44:40 +0300

    add start

--- app.js
+++ app.js
@@ -2,33 +2,5 @@
 var app = angular.module('scotcha', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/home');
-  $stateProvider
 
-  .state('home', {
-    url: '/home',
-    templateUrl: 'partial-home.html'
-  })
-  .state('home.list', {
-    url: '/list',
-    templateUrl: 'partial-home-list.html',
-    controller: function($scope) {
-      $scope.scotches = ['Balvenie', 'The Balmore', 'Highland Park'];
-    }
-  })
-  .state('scotch', {
-    url: '/scotch/:scotch',
-    templateUrl: function ($stateParams){
-      console.log($stateParams)
-      return 'partial-scotch-' + $stateParams.scotch + '.html';
-    }
-  })
-  .state('home.paragraph', {
-    url: '/paragraph',
-    templateUrl: 'partial-about-scotch.html'
-  })
-  .state('about', {
-    url: '/about',
-    templateUrl: 'partial-about.html'
-  });
 });
